a7bb1dcafd73bf88b0b69e112cb291d2956e7414
Robert Rumney <robertrumney@gmail.com>
2022-12-17 16:30:31 +0200

    V0.5
    - Added an Update1 function that applies an edge check when adjusting the terrain detail settings.
    - Added an Update2 function that adjusts the terrain detail settings without applying an edge check.
    - Updated the Update function to call either Update1 or Update2 based on the value of the applyEdgeCheck flag.
    - Added code to the Update1 function to set the terrain detail settings back to their default values when the player is within the edge

--- AdjustTerrainDetail.cs
+++ AdjustTerrainDetail.cs
@@ -1,28 +1,33 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+// This script adjusts the detail settings of terrains in the scene based on the player's position
 public class AdjustTerrainDetail : MonoBehaviour
 {
     #region Configuration
     // Minimum values for detailObjectDistance and heightmapPixelError
     public float minDetailObjectDistance = 30;
     public float minHeightmapPixelError = 3;
+
+    // List to store the default values for detailObjectDistance and heightmapPixelError for each terrain
+    private readonly List<(float, float)> defaultValues = new List<(float, float)>();
+
+    // Flag to determine whether to apply an edge check when adjusting the terrain detail settings
+    private readonly bool applyEdgeCheck = true;
     #endregion
 
     #region Object References
+    // List to store references to all the terrains in the scene
     [System.NonSerialized]
     List<Terrain> terrains;
 
+    // Reference to the player's transform
     [System.NonSerialized]
     Transform playerTransform;
     #endregion
 
     #region MonoBehaviours
-    private void Awake()
-    {
-        this.enabled = false;
-    }
-
+    // This function is called when the script is first enabled
     private void Start()
     {
         // Find all terrains in the scene
@@ -30,9 +35,70 @@ public class AdjustTerrainDetail : MonoBehaviour
 
         // Find the player's transform
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // Store the default values for detailObjectDistance and heightmapPixelError for each terrain
+        foreach (Terrain terrain in terrains)
+        {
+            defaultValues.Add((terrain.detailObjectDistance, terrain.heightmapPixelError));
+        }
     }
 
+    // This function is called every frame
     private void Update()
+    {
+        // Check if the edge check should be applied
+        if (applyEdgeCheck)
+        {
+            // If the edge check should be applied, call Update1
+            Update1();
+        }
+        else
+        {
+            // If the edge check should not be applied, call Update2
+            Update2();
+        }
+    }
+
+    // This function updates the terrain detail settings, applying an edge check to prevent the player from getting too close to the edges of the terrain
+    private void Update1()
+    {
+        // Loop through all the terrains in the scene
+        foreach (Terrain terrain in terrains)
+        {
+            // Calculate the distance from the player to the terrain
+            float distance = Vector3.Distance(playerTransform.position, terrain.transform.position);
+
+            // Calculate the edge buffer distance, which determines how close the player can get to the edges of the terrain
+            float edgeBuffer = 10;
+
+            // Check if the player's position is within the allowed range
+            if
+                (
+                    playerTransform.position.x < terrain.transform.position.x + terrain.terrainData.size.x / 2 - edgeBuffer &&
+                    playerTransform.position.x > terrain.transform.position.x - terrain.terrainData.size.x / 2 + edgeBuffer &&
+                    playerTransform.position.z < terrain.transform.position.z + terrain.terrainData.size.z / 2 - edgeBuffer &&
+                    playerTransform.position.z > terrain.transform.position.z - terrain.terrainData.size.z / 2 + edgeBuffer
+                )
+            {
+                // Player is within the edge buffer, so don't adjust the terrain detail settings
+                // Get the default values for the current terrain
+                (float defaultDetailObjectDistance, float defaultHeightmapPixelError) = defaultValues[terrains.IndexOf(terrain)];
+
+                // Set the terrain's detailObjectDistance and heightmapPixelError properties to their default values
+                terrain.detailObjectDistance = defaultDetailObjectDistance;
+                terrain.heightmapPixelError = defaultHeightmapPixelError;
+            }
+            else
+            {
+                // Update the terrain's detail settings based on the player's position
+                terrain.detailObjectDistance = Mathf.Max(distance * 0.5f, minDetailObjectDistance);
+                terrain.heightmapPixelError = Mathf.Max(distance * 0.1f, minHeightmapPixelError);
+            }
+        }
+    }
+
+
+    private void Update2()
     {
         // Adjust terrain detail settings based on the player's position
         foreach (Terrain terrain in terrains)
@@ -40,20 +106,9 @@ public class AdjustTerrainDetail : MonoBehaviour
             // Calculate the distance from the player to the terrain
             float distance = Vector3.Distance(playerTransform.position, terrain.transform.position);
 
-            // Check if the player is within a certain distance of the edges of the terrain
-            float edgeBuffer = 10;  // Adjust this value to control how close the player can get to the edges
-            if (playerTransform.position.x < terrain.transform.position.x + terrain.terrainData.size.x / 2 - edgeBuffer * 2 ||
-                playerTransform.position.x > terrain.transform.position.x - terrain.terrainData.size.x / 2 + edgeBuffer * 2 ||
-                playerTransform.position.z < terrain.transform.position.z + terrain.terrainData.size.z / 2 - edgeBuffer * 2 ||
-                playerTransform.position.z > terrain.transform.position.z - terrain.terrainData.size.z / 2 + edgeBuffer * 2)
-            {
-                // Player is within the edge buffer, so don't adjust the terrain detail settings
-                continue;
-            }
-
             // Update the terrain's detail settings based on the player's position
-            terrain.detailObjectDistance = Mathf.Max(distance * 0.5f, minDetailObjectDistance);
-            terrain.heightmapPixelError = Mathf.Max(distance * 0.1f, minHeightmapPixelError);
+            terrain.detailObjectDistance = distance * 0.5f;
+            terrain.heightmapPixelError = distance * 0.1f;
         }
     }
     #endregion
